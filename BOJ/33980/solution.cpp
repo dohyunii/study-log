71f676fe095dec5cf3c28b83841f1c474afeca6c
DoHyun Han <73400628+dohyunii@users.noreply.github.com>
2025-11-30 13:28:11 +0900

    Create solution.cpp

---
+++ BOJ/33980/solution.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+int solve(string s, string t){
+    int answer = 0;
+    auto update = [](string &s, const bool type, int &answer){
+        char fc = (type ? 'R' : 'U'), ec = (type ? 'U' : 'R');
+        int f = s.rfind(fc), e = s.find(ec);
+        if(s[0] != fc || s[n - 1] != ec){
+            ++answer;
+            if(f == n - 1 && !e){
+                swap(s[f], s[e]);
+            }
+            else if(e){
+                swap(s[e], s[n - 1]);
+            }
+            else{
+                swap(s[f], s[0]);
+            }
+        }
+    };
+    update(s, false, answer);
+    update(t, true, answer);
+    int mn = 1, curr = 0;
+    for(int i = 0; i < n - 1; ++i){
+        curr += (s[i] == 'U') - (t[i] == 'U');
+        mn = min(mn, curr);
+    }
+    answer += 1 - mn;
+    return answer;
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string s, t;
+    cin >> n;
+    n <<= 1;
+    cin >> s >> t;
+    // make first string U...R form, and make second string R...U form
+    cout << min(solve(s, t), solve(t, s));
+}
