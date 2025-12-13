7c272b85b61172c840fa63d21638d0d0040aaaa8
DoHyun Han <73400628+dohyunii@users.noreply.github.com>
2025-12-13 15:34:20 +0900

    Create solution.cpp

---
+++ atcoder/dp_g/solution.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int mxn = 1e5 + 5;
+vector<int> g[mxn];
+int dp[mxn];
+bool vis[mxn];
+void dfs(int v){
+    vis[v] = true;
+    int mx = 0;
+    for(auto i : g[v]){
+        if(!vis[i]){
+            dfs(i);
+        }
+        mx = max(mx, dp[i]);
+    }
+    dp[v] = mx + 1;
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, m, answer = 0;
+    cin >> n >> m;
+    for(int i = 0; i < m; ++i){
+        int u, v;
+        cin >> u >> v;
+        g[u].push_back(v);
+    }
+    for(int i = 1; i <= n; ++i){
+        if(!vis[i]){
+            dfs(i);
+            answer = max(answer, dp[i] - 1);
+        }
+    }
+    cout << answer;
+}
